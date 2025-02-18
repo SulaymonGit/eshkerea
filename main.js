2fa0c0e351516cec741ca206b55f79a05fd60a3d
SulaymonGit <buuusuuu7@gmail.com>
2025-02-18 10:17:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+ const mevalar = [
+     "Olma" , "Banan" , "Nok" ,"Olcha" , "Shaftoli" ,
+    [
+        "Cobalt" , "Malibu" , "Captiva" , "Nexia" ,
+         [
+            "BMW" , "MERS" , "Audi" , "Mustang" , 
+       ],
+         [
+         "Iphone" , "Samsung" , "Nokia" , "Redmi"
+        ]
+    ],
+    [
+ "Mars" , "ShukurBOt" , "NodirBot"
+     ]
+    
+ ];
+ console.log(mevalar [5]);
